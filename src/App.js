7a2a806cd230dcec69b7f13bb5efd20db9943bf1
Kingsley Okpo <kngsley2018@gmail.com>
2024-06-13 16:27:51 +0100

    first project commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes } from "react-router-dom"
+function App() {
+
+  return (
+    <div className='App'>
+      <Router >
+        <Routes>
+        </Routes>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
