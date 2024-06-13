b70904691ad57c074cc81730a3db4b7175751225
Kingsley Okpo <kngsley2018@gmail.com>
2024-06-13 18:12:51 +0100

    easter bridge international market

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
         <Routes>
         </Routes>
         <div className='background'>
-          <h1>Welcome To <br/>EasterBridgeMarket.com</h1>
+          <h1>EASTER BRIDGE INTERNATIONAL MARKET</h1>
+          <h3>Motto: EBIMU ...Uniting Africa through trade</h3>
+          <p> 
+
+
+Easter Bridge International Market is a world-class, state-of-the-art market plaza, catering to buyers and sellers of a wide range of commodities. It is a premier location and a meeting point for producers, manufacturers, importers, distributors, wholesalers, retailers, and service providers. This market offers unparalleled investment opportunities and stands as an enduring legacy. To participate, simply subscribe and contribute either in a lump sum or installments, and watch the development conclude within twelve months.</p>
         </div>
       </Router>
     </div>
